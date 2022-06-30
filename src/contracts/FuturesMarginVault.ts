4a873b7e6b50c125ab91ef7836c092371df57be6
David Vuong <david.vuong256@gmail.com>
2022-07-01 06:21:24 +1000

    fix: Resolve import errors

--- src/contracts/FuturesMarginVault.ts
+++ src/contracts/FuturesMarginVault.ts
@@ -1,4 +1,4 @@
-import FuturesMarginVaultArtifact from '../../artifacts/contracts/FuturesMarginVault.sol/FuturesMarginVault.json';
+import FuturesMarginVaultArtifact from '../../artifacts/contracts/FuturesPositionsManager.sol/FuturesPositionsManager.json';
 
 export const FuturesMarginVault = {
   address: '0x12c1562E3Bd84bb87283000bC278A9685E941950',

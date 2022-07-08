138df0ac126e52d028d49cc5fc48e27d6efe051a
David Vuong <david.vuong256@gmail.com>
2022-07-08 14:54:48 +1000

    rm: Remove manual testing files, use unit tests and UI to smoke

--- packages/core/src/contracts/PozitionManager.ts
+++
@@ -1,6 +0,0 @@
-import PozitionManagerArtifact from '../../../../artifacts/contracts/PozitionManager.sol/PozitionManager.json';
-
-export const PozitionManager = {
-  address: '0x12c1562E3Bd84bb87283000bC278A9685E941950',
-  abi: PozitionManagerArtifact.abi,
-};

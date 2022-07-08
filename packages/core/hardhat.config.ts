edae0ea367106b6cdb8ce2326fb4549cfb28dfff
David Vuong <david.vuong256@gmail.com>
2022-07-08 14:55:44 +1000

    feat+fix: Deployments are working with new contracts :)

--- packages/core/hardhat.config.ts
+++ packages/core/hardhat.config.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-import { HardhatUserConfig, task } from 'hardhat/config';
+import { HardhatUserConfig } from 'hardhat/config';
 import '@nomiclabs/hardhat-etherscan';
 import '@nomiclabs/hardhat-waffle';
 import '@typechain/hardhat';
@@ -8,23 +8,20 @@ import 'solidity-coverage';
 
 dotenv.config();
 
-// Sample `hardhat` task, similar to Gulp/Grunt.
-//
-// @see https://hardhat.org/guides/create-task.html
-task('accounts', 'Prints the list of accounts', async (_, hre) => {
-  const accounts = await hre.ethers.getSigners();
-  accounts.forEach((acc) => {
-    console.log(acc.address);
-  });
-});
-
 // @see: https://hardhat.org/config/ to learn more.
 const config: HardhatUserConfig = {
-  solidity: '0.8.4',
+  solidity: '0.8.11',
   networks: {
-    'optimism-kovan': {
-      url: process.env.OPTIMISM_KOVAN_URL!,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    local: {
+      url: 'http://localhost:8545',
+    },
+    'optimistic-kovan': {
+      url: process.env.NETWORK_ENDPOINT || 'https://kovan.optimism.io',
+      accounts: process.env.DEPLOYER_PRIVATE_KEY ? [process.env.DEPLOYER_PRIVATE_KEY] : [],
+    },
+    'optimistic-mainnet': {
+      url: process.env.NETWORK_ENDPOINT || 'https://mainnet.optimism.io',
+      accounts: process.env.DEPLOYER_PRIVATE_KEY ? [process.env.DEPLOYER_PRIVATE_KEY] : [],
     },
   },
   gasReporter: {
@@ -32,10 +29,7 @@ const config: HardhatUserConfig = {
     currency: 'USD',
   },
   etherscan: {
-    apiKey: {
-      // @see: `npx hardhat verify --list-networks`
-      optimisticKovan: process.env.ETHERSCAN_API_KEY!,
-    },
+    apiKey: process.env.ETHERSCAN_API_KEY!,
   },
 };
 
--- packages/core/scripts/deploy.ts
+++ packages/core/scripts/deploy.ts
@@ -1,34 +1,41 @@
-import './env';
-import { ethers } from 'hardhat';
+import { ethers, network } from 'hardhat';
+
+enum SupportedNetworks {
+  'optimistic-mainnet' = 'optimistic-mainnet',
+  'optimistic-kovan' = 'optimistic-kovan',
+}
 
 const main = async () => {
-  const baseURI = 'https://optimistic.etherscan.io/address';
-  const addresses = {
-    AddressResolver: '0x95A6a3f44a70172E7d50a9e28c85Dfd712756B8C',
+  // Network specific deployment configuration.
+  //
+  // @see: https://docs.synthetix.io/addresses/
+  const config = {
+    'optimistic-mainnet': {
+      baseURI: 'https://optimistic.etherscan.io/address',
+      addressResolver: '0x95A6a3f44a70172E7d50a9e28c85Dfd712756B8C',
+    },
+    'optimistic-kovan': {
+      baseURI: 'https://kovan-optimistic.etherscan.io/address',
+      addressResolver: '0xb08b62e1cdfd37eCCd69A9ACe67322CCF801b3A6',
+    },
   };
 
-  const FuturesNFTPositionF = await ethers.getContractFactory('FuturesNFTPosition');
-  const FuturesNFTPositionC = await FuturesNFTPositionF.deploy();
-  await FuturesNFTPositionC.deployed();
-  console.log(`Deployed FuturesNFTPosition at ${FuturesNFTPositionC.address}`);
+  const operatingNetwork = config[network.name as SupportedNetworks];
+  if (!operatingNetwork) {
+    throw new Error(`Missing configuration for network: '${network.name}'`);
+  }
 
-  const FuturesNFTPositionFactoryF = await ethers.getContractFactory('FuturesNFTPositionFactory');
-  const FuturesNFTPositionFactoryC = await FuturesNFTPositionFactoryF.deploy(FuturesNFTPositionC.address);
-  await FuturesNFTPositionFactoryC.deployed();
-  console.log(`Deployed FuturesNFTPositionFactory at ${FuturesNFTPositionFactoryC.address}`);
+  const { baseURI, addressResolver } = operatingNetwork;
 
-  const FuturesPositionsManagerF = await ethers.getContractFactory('FuturesPositionsManager');
+  const Pozition = await ethers.getContractFactory('Pozition');
+  const pozition = await Pozition.deploy();
+  await pozition.deployed();
+  console.log(`Deployed Pozition: ${baseURI}/${pozition.address} (${pozition.address})`);
 
-  const FuturesPositionsManagerC = await FuturesPositionsManagerF.deploy(
-    addresses.AddressResolver,
-    FuturesNFTPositionFactoryC.address
-  );
-  await FuturesPositionsManagerC.deployed();
-  console.log(`Deployed FuturesPositionsManager at ${FuturesPositionsManagerC.address}`);
-
-  [FuturesNFTPositionC, FuturesNFTPositionFactoryC, FuturesPositionsManagerC].forEach((contract) => {
-    console.log(`Deployed ${contract.constructor.name} to ${baseURI}/${contract.address}`);
-  });
+  const PozitionManager = await ethers.getContractFactory('PozitionManager');
+  const pozitionManager = await PozitionManager.deploy(addressResolver, pozition.address);
+  await pozitionManager.deployed();
+  console.log(`Deployed PozitionManager: ${baseURI}/${pozitionManager.address} (${pozitionManager.address})`);
 };
 
 main().catch((err) => {

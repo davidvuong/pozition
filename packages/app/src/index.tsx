bf3224588334f26c10fabb237ab33227ab4aaaa5
David Vuong <david.vuong256@gmail.com>
2022-07-10 11:47:37 +1000

    chore+feat: Install and configure Rainbowkit
    NOTE! Source maps generated in Webpack under react-scripts are busted.
    
    https://github.com/mswjs/msw/issues/1030
    
    GENERATE_SOURCEMAP=false

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,13 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./App";
 import { BrowserRouter } from "react-router-dom";
-
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { App } from "./App";
 import "./index.css";
 
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.optimism],
+  [alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Pozition",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <WagmiConfig client={wagmiClient}>
+    <RainbowKitProvider chains={chains}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      ,
+    </RainbowKitProvider>
+  </WagmiConfig>,
   document.getElementById("root")
 );

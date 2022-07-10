b0f12a445ba0e52119d849f3db58bb2e9a5d9662
David Vuong <david.vuong256@gmail.com>
2022-07-10 17:22:30 +1000

    chore: Add custom fonts

--- packages/app/tailwind.config.js
+++ packages/app/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        misto: ["Misto"],
+      },
       animation: {
         marquee: "marquee 25s linear infinite",
       },

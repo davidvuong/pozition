4a6e97af4608955a5a4a52f20b4b35e19d5bab7a
David Vuong <david.vuong256@gmail.com>
2022-07-24 19:09:23 +1000

    feat: Add no pozition/loading first time messages

--- packages/app/src/components/MyPozitionsTable.tsx
+++ packages/app/src/components/MyPozitionsTable.tsx
@@ -1,5 +1,8 @@
+import { RefreshIcon } from "@heroicons/react/solid";
+import classNames from "classnames";
 import { BigNumber, Contract, ethers } from "ethers";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useAccount, useNetwork, useSigner } from "wagmi";
 import { usePozitionContracts } from "../hooks/usePozitionContracts";
 import { useSynthetixContracts } from "../hooks/useSynthetixContracts";
@@ -19,6 +22,8 @@ export interface PozitionMetadata {
 }
 
 export const MyPozitionsTable = () => {
+  const navigate = useNavigate();
+
   // TODO: Move this into a context provider and share data across components.
   const [synthRates, setSynthRates] = useState<Record<string, BigNumber>>({});
   const { address } = useAccount();
@@ -99,11 +104,53 @@ export const MyPozitionsTable = () => {
 
   useEffect(() => {
     fetchPozitions();
+    setIsLoadingPozitions(true);
   }, [address, signer]);
 
+  const handleRefresh = async () => {
+    fetchPozitions();
+    fetchSynthRates();
+  };
+
+  const handleOpenNewPozition = () => navigate("/pozition");
+
+  if (pozitions.length === 0) {
+    if (isLoadingPozitions) {
+      return (
+        <div className="flex flex-col mt-16 text-gray-200 uppercase p-2 rounded-2xl">
+          <p>Hold on tight. Loading your Pozitions...</p>
+        </div>
+      );
+    } else {
+      return (
+        <div className="flex flex-col mt-16 text-gray-200 uppercase p-2 rounded-2xl">
+          <p>My guy... You have no Pozitions.</p>
+          <button
+            className="mt-8 p-2 bg-gray-800 uppercase rounded-lg hover:bg-gray-700"
+            onClick={() => handleOpenNewPozition()}
+          >
+            Open a new Pozition
+          </button>
+        </div>
+      );
+    }
+  }
+
   return (
-    <div className="shadow overflow-hidden rounded-lg max-w-7xl w-full mb-12">
-      <ul role="list" className="divide-y divide-gray-800 bg-black-700">
+    <div className="flex flex-col space-y-2 shadow overflow-hidden rounded-lg max-w-7xl w-full mb-12">
+      <button
+        type="button"
+        className="flex w-full justify-end"
+        onClick={() => handleRefresh()}
+      >
+        <RefreshIcon className="h-6 w-6 text-gray-200 hover:text-gray-400" />
+      </button>
+      <ul
+        role="list"
+        className={classNames("divide-y divide-gray-800 bg-black-700", {
+          "opacity-50": isLoadingPozitions,
+        })}
+      >
         {pozitions.map((pozition) => (
           <MyPozitionsTableRow
             key={pozition.contract.address}
